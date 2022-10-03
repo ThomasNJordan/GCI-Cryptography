ac5d6c95a7893cbca7701914be1d4ea23889b9ff
Thomas Jordan <71048363+stand4rd@users.noreply.github.com>
2022-10-03 12:16:26 -0700

    Add files via upload

---
+++ RSA_Optimized.py
@@ -0,0 +1,28 @@
+# This is a program to run a more optimized version of RSA to comprare time
+# with Shor's Algorithm.
+
+# https://cryptobook.nakov.com/asymmetric-key-ciphers/rsa-encrypt-decrypt-examples
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+
+keyPair = RSA.generate(3072)
+
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
+msg = b'A message for encryption'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg)
+print("Encrypted:", binascii.hexlify(encrypted))
+
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:', decrypted)
